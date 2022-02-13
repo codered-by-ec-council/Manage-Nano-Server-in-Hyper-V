d98fb9d59020c6b254e8cd00406ecd403f89a7a7
Emerson Candido <emerson.candido@live.com>
2022-02-13 20:31:33 +0000

    Install VMs
    Signed-off-by: Emerson Candido <emerson.candido@live.com>

--- Setup_Lab_Env/install_hyper-v.ps1
+++ Setup_Lab_Env/install_hyper-v.ps1
@@ -1,5 +1,27 @@
+# Set settings variables
+$VMS_PATH="C:"
+$VMS_DIR="${VM_PATH}/VMs"
+$VHD_PATH="${VMS_PATH}/${VMS_DIR}/Virtual Hard Disks/"
+$VM_SWITCH="PRIVATE"
+
 # Install Hyper-V role and required features for management (Restart may be required)
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+IF(!(Get-WindowsFeature -Name Hyper-V)){
+    Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+}
+
+# Create VMs directory
+IF(!(Test-Path ${VMS_DIR} )){
+    New-Item -ItemType Directory -Path $VMS_PATH -Name $VMS_DIR
+}
+
+# Set Default VMs Path
+Set-VMHost -VirtualMachinePath ${VMS_DIR}
+
+# Set Default Virtual Hard Disks Path
+Set-VMHost -VirtualHardDiskPath $VHD_PATH
+
+# Create the VM Switch
+New-VMSwitch -Name $VM_SWITCH -SwitchType Private 
 
 <# 
 References:
--- Setup_Lab_Env/install_vms.ps1
+++ Setup_Lab_Env/install_vms.ps1
@@ -1,25 +1,56 @@
-# 
+# VMs Creation 
 
+# Default settings
+$VMS_PATH       = "C:\VMs"
+$VMS_GENERATION = 2
+$VMS_PROCESSORS = 2
+
+# Declare VMs to be created
 $VMs = @()
 $VMsCounter ++
 
-$VMs += ,@($VMsCounter, 'DC01',1024,'dyn','yes',50)
+# Domain Controller
+$VMs += ,@($VMsCounter, 'DC01',50,'dyn',1024,3072)
 $VMsCounter ++
 
-$VMs += ,@($VMsCounter, 'WEB01',512,'dyn','yes',10)
+# Hyper-V 01 Nano Server node
+$VMs += ,@($VMsCounter, 'HV01',0,'stat',2048)
 $VMsCounter ++
 
-$VMs += ,@($VMsCounter, 'DNS01',512,'dyn','yes',10)
+# Hyper-V 02 Nano Server node
+$VMs += ,@($VMsCounter, 'HV02',0,'sta',2048)
 $VMsCounter ++
 
-$VMs += ,@($VMsCounter, 'HV01',2048,'sta','yes',100)
-$VMsCounter ++
-
-$VMs += ,@($VMsCounter, 'HV01',2048,'sta','yes',100)
-$VMsCounter ++
-
-
+# Start the creation of VMs
 foreach($VM in $VMs){
+    # Populate VM Variable
+    $VM_NAME      = $VM[1]
+    $VM_VHD_SIZE  = [int64]$VM[2] * 1024 * 1024 * 1024
+    $VM_MEM_TYPE  = $VM[3]
+    $VM_MEM_START = [int64]$VM[4] * 1024 * 1024
 
-    Write-host ($VM)
+    # Create the VM if not exists
+    IF(!(Get-VM -Name $VM_NAME -ErrorAction SilentlyContinue)){
+ 
+        # Create VM with VHD
+        IF($VM_VHD_SIZE -ne 0){
+            New-VM -Name $VM_NAME `
+                -MemoryStartupBytes ${VM_MEM_START} `
+                -Path $VMS_PATH `
+                -Generation $VMS_GENERATION `
+                -NewVHDPath "${VMS_PATH}/${VM_NAME}/Virtual Hard Disks/${VM_NAME}_OS.vhdx" `
+                -NewVHDSizeBytes $VM_VHD_SIZE
+            Pause
+        }
+    
+        # Create VM without VHD
+        IF($VM_VHD_SIZE -eq 0){
+            New-VM -Name $VM_NAME `
+                -MemoryStartupBytes ${VM_MEM_START} `
+                -Path $VMS_PATH `
+                -Generation $VMS_GENERATION `
+                -NoVHD
+            Pause
+        }
+    }
 }
