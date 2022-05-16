c5053c5e9cb57c98f71a4e39598a00ed975c5eba
Emerson Candido <emerson.candido@live.com>
2022-05-16 09:03:31 +0200

    Fix lab4 resolution

--- LABs/Section 4/lab_resolution_Create_a_Hyper-V_cluster.ps1
+++ LABs/Section 4/lab_resolution_Create_a_Hyper-V_cluster.ps1
@@ -5,14 +5,8 @@ $VM_NAME     = "ECCOUNCIL_SCVMM"
 
 # Cluster settings
 $CLUSTER_NAME  = "nanoCluster" # Name of the cluster
-$CLUSTER_NODES = "hv01,hv02" # Name of the nodes that composes the cluster
+$CLUSTER_NODES = ("hv01","hv02") # Name of the nodes that composes the cluster
 $CLUSTER_IP    = "10.0.0.250" # IP of cluster
-
-# Share settings
-$SHARE_PATH       = "V:\VMs" # Path of the share
-$SHARE_NAME       = "VMs" # Name of the share
-$SHARE_FULLACCESS = ("eccouncil\HV01$", "eccouncil\HV02$", "eccouncil\nanoCluster$", "eccouncil\Domain Admins", "Administrators") # Access of the share
-
 
 # Create authentication
 $VM_PASSWORD_SEC = ConvertTo-SecureString -String $VM_PASSWORD -AsPlainText -Force
@@ -34,20 +28,8 @@ IF(!$error) {
             $CLUSTER_NAME     = $args[0]
             $CLUSTER_NODES    = $args[1]
             $CLUSTER_IP       = $args[2]
-            $SHARE_PATH       = $args[3]
-            $SHARE_NAME       = $args[4]
-            $SHARE_FULLACCESS = $args[5]
-
+            
             # Create the cluster
             New-Cluster -Name $CLUSTER_NAME -Node $CLUSTER_NODES -NoStorage -StaticAddress $CLUSTER_IP
-                    
-            # Create folder
-            New-Item -ItemType Directory -Path $SHARE_PATH
-
-            # Create file share
-            New-SmbShare -Name $SHARE_NAME -Path $SHARE_PATH -FullAccess $SHARE_FULLACCESS
-
-            # Set NTFS permissions from the file share permissions
-            Set-SmbPathAcl -ShareName $SHARE_NAME
         }
 }
